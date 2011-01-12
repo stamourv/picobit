ccfe458e6f315ceba193e202834d99f687eeec81
Jim Shargo <mnudev@gmail.com>
2011-01-12 16:40:50 -0500

    Introduces automatic testing script.

---
+++ run-tests.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require rackunit)
+
+(for [(file (in-directory "./tests/"))]
+  (when (regexp-match? #rx"[.]scm$" file)
+    (let* [(file-str (path->string file))
+           (base (substring file-str 0 (- (string-length file-str) 4)))
+           (hex (string-append base ".hex"))
+           (expected (string-append base ".expected"))]
+      (if (file-exists? expected)
+          (dynamic-wind 
+           (λ () (system* "./picobit" file-str))
+           (λ () (if (file-exists? hex)
+                     (check-equal? (file->string (string->path expected))
+                                   (file->string (string->path hex))
+                                   base)
+                     (printf "~a did not compile!" file-str)))
+           (λ () (if (file-exists? hex) 
+                     (delete-file hex)
+                     '())))
+          (printf "File ~a has no expected file!\n" file-str)))))
