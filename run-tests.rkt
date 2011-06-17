08357f1778e6f20e566cb0b32d375b6125afc003
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-17 14:32:32 -0400

    Slight cleanup of the testing script.

--- run-tests.rkt
+++ run-tests.rkt
@@ -2,21 +2,23 @@
 
 (require rackunit)
 
-(for [(file (in-directory "./tests/"))]
-  (when (regexp-match? #rx"[.]scm$" file)
-    (let* [(file-str (path->string file))
-           (base (substring file-str 0 (- (string-length file-str) 4)))
-           (hex (string-append base ".hex"))
-           (expected (string-append base ".expected"))]
-      (if (file-exists? expected)
-          (dynamic-wind 
-           (λ () (system* "./picobit" file-str))
-           (λ () (if (file-exists? hex)
-                     (check-equal? (file->string (string->path expected))
-                                   (file->string (string->path hex))
-                                   base)
-                     (printf "~a did not compile!" file-str)))
-           (λ () (if (file-exists? hex) 
-                     (delete-file hex)
-                     '())))
-          (printf "File ~a has no expected file!\n" file-str)))))
+(for ([file (in-directory "./tests/")]
+      #:when (and (regexp-match? #rx"[.]scm$" file)
+                  ;; skip emacs temp unsaved file backups
+                  (not (regexp-match "^\\.#" file))))
+  (let* [(file-str (path->string file))
+         (base (substring file-str 0 (- (string-length file-str) 4)))
+         (hex (string-append base ".hex"))
+         (expected (string-append base ".expected"))]
+    (if (file-exists? expected)
+        (dynamic-wind
+          (λ () (system* "./picobit" file-str))
+          (λ () (if (file-exists? hex)
+                    (check-equal? (file->string (string->path expected))
+                                  (file->string (string->path hex))
+                                  base)
+                    (printf "~a did not compile!\n" file-str)))
+          (λ () (if (file-exists? hex)
+                    (delete-file hex)
+                    '())))
+        (printf "File ~a has no expected file!\n" file-str))))
