ae58223ee8ef2353a1bb17c040bde32cd7c82251
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-19 17:15:56 -0400

    Change test harness to show test summary.
    At this point, we pass 9 out of 30.
    (That is, 9 out of 30 give the exact same bytecode as the gambit-based
    picobit. More than 9 tests produce bytecode that has the same behavior.)

--- run-tests.rkt
+++ run-tests.rkt
@@ -1,23 +1,29 @@
 #lang racket
 
-(require rackunit)
+(require rackunit rackunit/text-ui)
 
-(for ([file (in-directory "./tests/")]
-      #:when (and (regexp-match? #rx"[.]scm$" file)
-                  ;; skip emacs temp unsaved file backups
-                  (not (regexp-match "^\\.#" file))))
-  (let* [(file-str (path->string file))
-         (hex (path-replace-suffix file ".hex"))
-         (expected (path-replace-suffix file ".expected"))]
-    (if (file-exists? expected)
-        (dynamic-wind
-          (λ () (system* "./picobit" file-str))
-          (λ () (if (file-exists? hex)
-                    (check-equal? (file->string hex)
-                                  (file->string expected)
-                                  file-str)
-                    (printf "~a did not compile!\n" file-str)))
-          (λ () (if (file-exists? hex)
-                    (delete-file hex)
-                    '())))
-        (printf "File ~a has no expected file!\n" file-str))))
+(void
+ (run-tests
+  (make-test-suite
+   "PICOBIT tests"
+   (for/list ([file (in-directory "./tests/")]
+              #:when (and (regexp-match? #rx"[.]scm$" file)
+                          ;; skip emacs temp unsaved file backups
+                          (not (regexp-match "^\\.#" file))))
+
+     (let* ([file-str (path->string file)]
+            [hex (path-replace-suffix file ".hex")]
+            [expected (path-replace-suffix file ".expected")])
+       (test-suite
+        file-str
+        (if (file-exists? expected)
+            (dynamic-wind
+              (λ () (system* "./picobit" file-str))
+              (λ () (if (file-exists? hex)
+                        (check-equal? (file->string hex)
+                                      (file->string expected))
+                        (printf "~a did not compile!\n" file-str)))
+              (λ () (if (file-exists? hex)
+                        (delete-file hex)
+                        '())))
+            (printf "File ~a has no expected file!\n" file-str))))))))
