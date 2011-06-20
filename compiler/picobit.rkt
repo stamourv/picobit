f2112b7e8e75eeaa4eb3d6888cc9c54facb2fcda
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-19 23:05:58 -0400

    Put compiler files in their own directory.

--- compiler/picobit.rkt
+++ compiler/picobit.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require (only-in unstable/port read-all))
+(require (only-in unstable/port read-all)
+         racket/runtime-path)
 (require "utilities.rkt"
          "ast.rkt"
          "env.rkt"
@@ -23,9 +24,12 @@
              e))
        exprs))
 
+(define-runtime-path compiler-dir ".")
+
 (define (parse-file filename)
   (let* ((library
-          (with-input-from-file "library.scm" read-all))
+          (with-input-from-file (build-path compiler-dir "library.scm")
+            read-all))
          (toplevel-exprs
           (expand-includes
            (append library
