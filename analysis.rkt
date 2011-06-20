26c5bd03a68ab98b6c6d244e1361df3e800801bb
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-19 22:22:03 -0400

    Get rid of varset utilities.

--- analysis.rkt
+++ analysis.rkt
@@ -42,64 +42,47 @@
 
 (require (except-in racket/set
                     set? set)) ; to avoid collision with the node type
-(define (varset-empty)                  (seteq))
-(define (varset-singleton x)            (seteq x))
-(define (list->varset lst)              (list->seteq lst))
-(define (varset->list set)              (set->list set))
-(define (varset-size set)               (set-count set))
-(define (varset-empty? set)             (set-empty? set))
-(define (varset-member? x set)          (set-member? set x))
-(define (varset-adjoin set x)           (set-add set x))
-(define (varset-remove set x)           (set-remove set x))
-(define (varset-equal? s1 s2)           (equal? s1 s2))
-(define (varset-subset? s1 s2)          (subset? s1 s2))
-(define (varset-difference set1 set2)   (set-subtract set1 set2))
-(define (varset-union set1 set2)        (set-union set1 set2))
-(define (varset-intersection set1 set2) (set-intersect set1 set2))
-(define (varset-union-multi sets)       (apply set-union sets))
+
+;; varsets are eq? sets
 
 (define (build-params-varset params)
-  (list->varset params))
+  (list->seteq params))
 
 ;; These two are for outside consumption, so they return results as lists.
 (define (global-fv node)
   (filter var-global?
-          (varset->list (fv node))))
+          (set->list (fv node))))
 (define (non-global-fv node)
   (filter (lambda (x) (not (var-global? x)))
-          (varset->list (fv node))))
+          (set->list (fv node))))
 
 (define (fv node)
   (cond ((cst? node)
-         (varset-empty))
+         (seteq)) ; empty varset
         ((ref? node)
          (let ((var (ref-var node)))
-           (varset-singleton var)))
+           (seteq var))) ; singleton varset
         ((def? node)
          (let ((var (def-var node))
                (val (child1 node)))
-           (varset-union
-            (varset-singleton var)
-            (fv val))))
+           (set-add (fv val) var)))
         ((set? node)
          (let ((var (set-var node))
                (val (child1 node)))
-           (varset-union
-            (varset-singleton var)
-            (fv val))))
+           (set-add (fv val) var)))
         ((if*? node)
          (let ((a (list-ref (node-children node) 0))
                (b (list-ref (node-children node) 1))
                (c (list-ref (node-children node) 2)))
-           (varset-union-multi (list (fv a) (fv b) (fv c)))))
+           (set-union (fv a) (fv b) (fv c))))
         ((prc? node)
          (let ((body (list-ref (node-children node) 0)))
-           (varset-difference
+           (set-subtract
             (fv body)
             (build-params-varset (prc-params node)))))
         ((call? node)
-         (varset-union-multi (map fv (node-children node))))
+         (apply set-union (map fv (node-children node))))
         ((seq? node)
-         (varset-union-multi (map fv (node-children node))))
+         (apply set-union (map fv (node-children node))))
         (else
          (compiler-error "unknown expression type" node))))
