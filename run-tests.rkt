4827596c60985a40ce620d4067f405566ad9ff59
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-22 20:40:21 -0400

    Allow running individual tests.

--- run-tests.rkt
+++ run-tests.rkt
@@ -7,29 +7,37 @@
 ;; regressions. Checking for bytecode equality would be too strong an
 ;; equivalence.
 ;; This should be fixed.
+
+(define (run-one file)
+  (let* ([file-str (path->string file)]
+         [hex (path-replace-suffix file ".hex")]
+         [expected (path-replace-suffix file ".expected")])
+    (test-suite
+     file-str
+     (begin
+       (test-case "no expected file" (check-true (file-exists? expected)))
+       (when (file-exists? expected)
+         (system* "./picobit" file-str)
+         (test-case "compilation" (check-true (file-exists? hex)))
+         (when (file-exists? hex)
+           (let ([out (with-output-to-string
+                        (lambda ()
+                          (system* "./picobit-vm" hex)))])
+             (test-case "execution"
+                        (check-equal? out (file->string expected))))
+           (delete-file hex)))))))
+
+(define args (current-command-line-arguments))
+
 (void
  (run-tests
-  (make-test-suite
-   "PICOBIT tests"
-   (for/list ([file (in-directory "./tests/")]
-              #:when (and (regexp-match? #rx"[.]scm$" file)
-                          ;; skip emacs temp unsaved file backups
-                          (not (regexp-match "^\\.#" file))))
-     
-     (let* ([file-str (path->string file)]
-            [hex (path-replace-suffix file ".hex")]
-            [expected (path-replace-suffix file ".expected")])
-       (test-suite
-        file-str
-        (begin
-          (test-case "no expected file" (check-true (file-exists? expected)))
-          (when (file-exists? expected)
-            (system* "./picobit" file-str)
-            (test-case "compilation" (check-true (file-exists? hex)))
-            (when (file-exists? hex)
-              (let ([out (with-output-to-string
-                           (lambda ()
-                             (system* "./picobit-vm" hex)))])
-                (test-case "execution"
-                           (check-equal? out (file->string expected))))
-              (delete-file hex))))))))))
+  (cond [(>= (vector-length args) 1) ; run one
+         (run-one (string->path (vector-ref args 0)))]
+        [else ; run all
+         (make-test-suite
+          "PICOBIT tests"
+          (for/list ([file (in-directory "./tests/")]
+                     #:when (and (regexp-match? #rx"[.]scm$" file)
+                                 ;; skip emacs temp unsaved file backups
+                                 (not (regexp-match "^\\.#" file))))
+            (run-one file)))])))
