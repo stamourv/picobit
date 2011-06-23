2d408c6e60261f7ed92d3e80c7dda99938767ff3
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-23 18:30:50 -0400

    Add a failing test for interaction between GC and bignum display.

---
+++ tests/succeed/gc-bignum-display.scm
@@ -0,0 +1,14 @@
+;; bug: after GC, bignum display becomes odd
+
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
+(displayln (* 21435 2141241))
