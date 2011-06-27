17e10dc31dcd720e5f9dc34097ed5ab510ee37e3
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-27 09:51:54 -0400

    Reformat code-gen.rkt.

--- compiler/code-gen.rkt
+++ compiler/code-gen.rkt
@@ -7,72 +7,69 @@
 ;; Each of these adds an IR instruction to the code stream.
 
 (define (gen-instruction instr nb-pop nb-push ctx)
-  (let* ((env
-          (context-env ctx))
-         (stk
-          (stack-extend #f
-                        nb-push
-                        (stack-discard nb-pop
-                                       (env-local env)))))
+  (let* ([env (context-env ctx)]
+         [stk (stack-extend #f
+                            nb-push
+                            (stack-discard nb-pop (env-local env)))])
     (context-add-instr (context-change-env ctx (env-change-local env stk))
                        instr)))
 
 (define (gen-entry nparams rest? ctx)
-  (gen-instruction (list 'entry nparams rest?) 0 0 ctx))
+  (gen-instruction `(entry ,nparams ,rest?) 0 0 ctx))
 
 (define (gen-push-constant val ctx)
-  (gen-instruction (list 'push-constant val) 0 1 ctx))
+  (gen-instruction `(push-constant ,val) 0 1 ctx))
 
 (define (gen-push-unspecified ctx)
   (gen-push-constant #f ctx))
 
 (define (gen-push-local-var var ctx)
-  (let ((i (find-local-var var (context-env ctx))))
+  (let ([i (find-local-var var (context-env ctx))])
     (if (>= i 0)
         (gen-push-stack i ctx)
-        (gen-push-stack
+        (gen-push-stack ; in the closed env, past the local variables
          (+ (- -1 i)
             (length (stack-slots (env-local (context-env ctx))))) ctx))))
 
 (define (gen-push-stack pos ctx)
-  (gen-instruction (list 'push-stack pos) 0 1 ctx))
+  (gen-instruction `(push-stack ,pos) 0 1 ctx))
 
 (define (gen-push-global var ctx)
-  (gen-instruction (list 'push-global var) 0 1 ctx))
+  (gen-instruction `(push-global ,var) 0 1 ctx))
 
 (define (gen-set-global var ctx)
-  (gen-instruction (list 'set-global var) 1 0 ctx))
+  (gen-instruction `(set-global ,var) 1 0 ctx))
 
 (define (gen-call nargs ctx)
-  (gen-instruction (list 'call nargs) (+ nargs 1) 1 ctx))
+  (gen-instruction `(call ,nargs) (+ nargs 1) 1 ctx))
 
 (define (gen-jump nargs ctx)
-  (gen-instruction (list 'jump nargs) (+ nargs 1) 1 ctx))
+  (gen-instruction `(jump ,nargs) (+ nargs 1) 1 ctx))
 
 (define (gen-call-toplevel nargs id ctx)
-  (gen-instruction (list 'call-toplevel id) nargs 1 ctx))
+  (gen-instruction `(call-toplevel ,id) nargs 1 ctx))
 
 (define (gen-jump-toplevel nargs id ctx)
-  (gen-instruction (list 'jump-toplevel id) nargs 1 ctx))
+  (gen-instruction `(jump-toplevel ,id) nargs 1 ctx))
 
 (define (gen-goto label ctx)
-  (gen-instruction (list 'goto label) 0 0 ctx))
+  (gen-instruction `(goto ,label) 0 0 ctx))
 
 (define (gen-goto-if-false label-false label-true ctx)
-  (gen-instruction (list 'goto-if-false label-false label-true) 1 0 ctx))
+  (gen-instruction `(goto-if-false ,label-false ,label-true) 1 0 ctx))
 
 (define (gen-closure label-entry ctx)
-  (gen-instruction (list 'closure label-entry) 1 1 ctx))
+  (gen-instruction `(closure ,label-entry) 1 1 ctx))
 
 (define (gen-prim id nargs unspec-result? ctx)
-  (gen-instruction (list 'prim id)
+  (gen-instruction `(prim ,id)
                    nargs
                    (if unspec-result? 0 1)
                    ctx))
 
 (define (gen-pop ctx)
-  (gen-instruction (list 'pop) 1 0 ctx))
+  (gen-instruction '(pop) 1 0 ctx))
 
 (define (gen-return ctx)
-  (let ((ss (stack-size (env-local (context-env ctx)))))
-    (gen-instruction (list 'return) ss 0 ctx)))
+  (let ([ss (stack-size (env-local (context-env ctx)))])
+    (gen-instruction '(return) ss 0 ctx)))
