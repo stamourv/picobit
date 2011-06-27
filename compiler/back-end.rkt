dab45732a994fd792dee572fad23db12291ee814
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-26 23:41:24 -0400

    Refactor back-end.rkt.

--- compiler/back-end.rkt
+++ compiler/back-end.rkt
@@ -9,59 +9,34 @@
 (provide renumber-labels)
 
 (define (renumber-labels bbs ref-counts n)
-  (let ((new-bbs (make-vector n)))
-    (let loop2 ((label 0))
-      (if (< label (vector-length bbs))
-          (if (> (vector-ref ref-counts label) 0)
-              (let* ((bb (vector-ref bbs label))
-                     (new-label (bb-label bb))
-                     (rev-instrs (bb-rev-instrs bb)))
 
-                (define fix
-                  (lambda (instr)
+  (define (fix instr)
+    (define (make-new-label label)
+      (bb-label (vector-ref bbs label)))
+    (match instr
+      [`(,(and opcode (or 'closure 'call-toplevel 'jump-toplevel 'goto)) ,arg)
+       (list opcode (make-new-label arg))]
+      [`(goto-if-false ,a1 ,a2)
+       (list 'goto-if-false (make-new-label a1) (make-new-label a2))]
+      [_ instr]))
 
-                    (define new-label
-                      (lambda (label)
-                        (bb-label (vector-ref bbs label))))
-
-                    (let ((opcode (car instr)))
-                      (cond ((eq? opcode 'closure)
-                             (list 'closure
-                                   (new-label (cadr instr))))
-                            ((eq? opcode 'call-toplevel)
-                             (list 'call-toplevel
-                                   (new-label (cadr instr))))
-                            ((eq? opcode 'jump-toplevel)
-                             (list 'jump-toplevel
-                                   (new-label (cadr instr))))
-                            ((eq? opcode 'goto)
-                             (list 'goto
-                                   (new-label (cadr instr))))
-                            ((eq? opcode 'goto-if-false)
-                             (list 'goto-if-false
-                                   (new-label (cadr instr))
-                                   (new-label (caddr instr))))
-                            (else
-                             instr)))))
-
-                (vector-set!
-                 new-bbs
-                 new-label
-                 (make-bb new-label (map fix rev-instrs)))
-                (loop2 (+ label 1)))
-              (loop2 (+ label 1)))
-          new-bbs))))
+  (let ([new-bbs (make-vector n)])
+    (for ([(b label) (in-indexed bbs)]
+          #:when (> (vector-ref ref-counts label) 0))
+      (match b
+        [(bb new-label rev-instrs)
+         (vector-set! new-bbs new-label
+                      (make-bb new-label (map fix rev-instrs)))]))
+    new-bbs))
 
 ;-----------------------------------------------------------------------------
 
 (provide code->vector)
 
 (define (code->vector code)
-  (let ((v (make-vector (+ (code-last-label code) 1))))
-    (for-each
-     (lambda (bb)
-       (vector-set! v (bb-label bb) bb))
-     (code-rev-bbs code))
+  (let ([v (make-vector (+ (code-last-label code) 1))])
+    (for ([bb (in-list (code-rev-bbs code))])
+      (vector-set! v (bb-label bb) bb))
     v))
 
 ;-----------------------------------------------------------------------------
