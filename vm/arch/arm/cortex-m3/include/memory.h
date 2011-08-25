28d30efcde1ef1d9cf1fd338c64ff47fa6596c51
Peter Zotov <whitequark@whitequark.org>
2011-08-25 05:02:18 +0400

    Fix linking.

--- vm/arch/arm/cortex-m3/include/memory.h
+++ vm/arch/arm/cortex-m3/include/memory.h
@@ -3,10 +3,10 @@
 
 #define CODE_START 0x8000
 
-extern uint8 __picobit_ram;
+extern uint8 __picobit_heap;
 
-#define ram_get(a) ((uint8*)&__picobit_ram)[a]
-#define ram_set(a,x) ((uint8*)&__picobit_ram)[a] = (x)
+#define ram_get(a) ((uint8*)&__picobit_heap)[a]
+#define ram_set(a,x) ((uint8*)&__picobit_heap)[a] = (x)
 
 #define rom_get(a) (((uint8*) 0)[a])
 
