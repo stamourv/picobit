d7eac0e3b376b2d470cdad8978919ce6524db6ae
Peter Zotov <whitequark@whitequark.org>
2011-09-10 01:04:24 +0400

    Don't try to include #ifdef-ed out primitives to the list.

--- vm/include/primitives.h
+++ vm/include/primitives.h
@@ -5,10 +5,15 @@
 extern const char* const primitive_names[];
 #endif /* CONFIG_DEBUG_STRINGS */
 
+/* For the primitive scanning pass. */
+#ifndef NO_PRIMITIVE_EXPAND
+
 #define PRIMITIVE(scheme_name, c_name, args) \
 	void prim_ ## c_name ()
 
 #define PRIMITIVE_UNSPEC PRIMITIVE
+
+#endif /* NO_PRIMITIVE_EXPAND */
 
 /* Temporary storage for primitives */
 extern obj a1, a2, a3;
